436be6f34e221348f235cb69aa9059dab0a8aca5
nantawat charoenrat <vss555mos@gmail.com>
2024-02-05 13:32:46 +0700

    05/2/2024/13:32

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,10 +1,36 @@
 import { useRouter } from "next/router"
-export default function ProductDetail(){
-    const router = useRouter();
-    const {id} = router.query;
-    return(
+
+export async function getStaticPaths() {
+    const res = await fetch("https://dummyjson.com/products?limit=10");
+    const data = await res.json();
+    const paths = data.products.map((item) => {
+        return {
+            params: { id: String(item.id) }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    const id = params.id;
+    const res = await fetch("https://dummyjson.com/products/"+id);
+    const data = await res.json();
+    console.log(data);
+    return {
+        props: { product: data },
+    };
+}
+
+
+export default function ProductDetail({product}) {
+
+    return (
         <>
-            <h1>สิ้นค้าชั้นที่ {id} แบบเบิ้มๆ</h1>
+            <h1>สินค้าชิ้นที่ { product.id} </h1>
+            <h1>ชื่อสินค้า { product.title} </h1>
         </>
     )
 }
